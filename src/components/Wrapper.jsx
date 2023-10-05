34e7a5fa015a0dd68b8bc4bf56314519bc85e2ed
Andy-Vasseur <beaugosswwe@live.fr>
2023-10-06 01:24:32 +0200

    :art: | change the app name

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,7 +1,7 @@
 function Wrapper() {
     return (
         <div className="Wrapper">
-            <h1>Border Radius Generator</h1>
+            <h1>EasyRadius Generator</h1>
             <p>a service made by Andy Vasseur</p>
             <p>version 0.0.1</p>
         </div>
