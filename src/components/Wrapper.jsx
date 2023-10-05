716c2d6283f763a4389304cd643ba5127360cc98
Andy-Vasseur <beaugosswwe@live.fr>
2023-10-06 01:28:34 +0200

    :construction: | updated app version

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -3,7 +3,7 @@ function Wrapper() {
         <div className="Wrapper">
             <h1>EasyRadius Generator</h1>
             <p>a service made by Andy Vasseur</p>
-            <p>version 0.0.1</p>
+            <p>version 0.0.8</p>
         </div>
     )
 }
