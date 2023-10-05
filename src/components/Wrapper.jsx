059ccb65efb5f8d53892b9e235f7ee393ff387be
Andy-Vasseur <beaugosswwe@live.fr>
2023-10-06 01:05:09 +0200

    :art: | added some favicons and version number

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -3,6 +3,7 @@ function Wrapper() {
         <div className="Wrapper">
             <h1>Border Radius Generator</h1>
             <p>a service made by Andy Vasseur</p>
+            <p>version 0.0.1</p>
         </div>
     )
 }
